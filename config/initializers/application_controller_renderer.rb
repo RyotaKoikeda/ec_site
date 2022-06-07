b4ec63f8ebcf9bae647a115f7f73d39b920db2b8
Ryota Koikeda <ryota0706ryota@gmail.com>
2022-06-08 03:10:01 +0900

    init

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '906054a9ee8301cf9e713652957aa7aaeb06491ffcda72e33b6bb4ca3d51ee265fa4d5825818dbe2f8ff7ceff68797b26383976533b79c1a01cc48df50972c63'
